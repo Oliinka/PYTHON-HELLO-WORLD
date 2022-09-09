215ebc30ce1d527fb52ee9e67c6b997f17f986db
Oliinka <111246053+Oliinka@users.noreply.github.com>
2022-09-09 16:33:06 +0200

    Add files via upload

---
+++ 2_19_dictionary.py
@@ -0,0 +1,8 @@
+customer = {
+    "name": "John Smith",
+    "age": 30,
+    "is_verified": True
+}
+#customer["name"] = "Jack Smith"
+#customer["birthdate"] = "Jan 1 1980"
+print(customer.get("birthdate", "Jan 1 1980"))
